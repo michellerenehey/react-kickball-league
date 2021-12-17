71931617f616b58b0ad7b908ce4315d6ecd0fd68
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-17 15:18:48 -0800

    added loading to players

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -5,15 +5,18 @@ import PlayerList from '../../components/Player/PlayerList';
 
 export default function Players() {
   const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPlayers();
       setPlayers(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
 
+  if (loading) return <h3>...Loading</h3>;
   return (
     <div>
       {players.map((player) => (
