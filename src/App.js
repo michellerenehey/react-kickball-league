1121301d927bcc16c61cdb4086a784f64ebc7c2d
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-16 16:22:12 -0800

    added player and team to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, NavLink, Route } from 'react-router-dom';
 
 import Teams from './views/Teams/Teams';
 import Players from './views/Players/Players';
+import Team from './views/Team/Team';
+import Player from './views/Player/Player';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
         <NavLink to="/players">Players</NavLink>
         <Switch>
           <Route exact path="/teams" component={Teams} />
+          <Route exact path="/teams/:id" component={Team} />
           <Route exact path="/players" component={Players} />
+          <Route exact path="/players/:id" component={Player} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/views/Player/Player.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Player() {
+  return <div>i am a Player (singular) component</div>;
+}
