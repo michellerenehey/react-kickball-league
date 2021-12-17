41620b05d4fa197297d89e2ad657b8bd4df6e741
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-16 17:38:28 -0800

    added Home component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Teams from './views/Teams/Teams';
 import Players from './views/Players/Players';
 import Team from './views/Team/Team';
 import Player from './views/Player/Player';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <h1>Kickball Manager</h1>
+
       <BrowserRouter>
         <NavLink to="/teams">Teams</NavLink>
         <NavLink to="/players">Players</NavLink>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/teams" component={Teams} />
           <Route exact path="/teams/:id" component={Team} />
           <Route exact path="/players" component={Players} />
