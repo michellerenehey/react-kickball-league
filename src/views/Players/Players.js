d8dcd64dd7e55ba4f39377c0fcf11460cafe4d1f
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-17 12:56:42 -0800

    set players state to do data fetch for players

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchPlayers } from '../../services/players';
 
 export default function Players() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPlayers();
+      setPlayers(data);
+    };
+    fetchData();
+  }, []);
+  console.log(players);
+
   return <div>i am the Players component</div>;
 }
